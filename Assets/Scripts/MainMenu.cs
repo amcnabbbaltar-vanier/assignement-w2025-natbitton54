b1f240b1c4edfffe1e687b61cdbce7e77939a616
natbitton54 <nathanieldavidbitton54@gmail.com>
2025-02-16 22:20:25 -0500

    fixed

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -13,8 +13,8 @@ public class MainMenuScript : MonoBehaviour
     {
         Application.Quit();
 
-#if UNITY_EDITOR
-        UnityEditor.EditorApplication.isPlaying = false;
-#endif
+        #if UNITY_EDITOR
+                UnityEditor.EditorApplication.isPlaying = false;
+        #endif
     }
 }
